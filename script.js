051e6150fa72c6b8d0e2b5dba9239f4efe0f5f8e
STALKER Legioner <seyfulla.ismm@gmail.com>
2024-05-30 18:19:08 +0400

    math working

---
+++ script.js
@@ -0,0 +1,72 @@
+// var bucaqA = +prompt ('Üçbucağın bir bucağını qeyd edin')
+
+// var bucaqB = +prompt ('Üçbucağın ikinici bucağını qeyd edin')
+
+// var cem = bucaqA + bucaqB
+
+
+// if(cem<180){
+//     console.log("üçüncü bucaq bərabərdir" +  " " + (180-cem));
+// }
+// else {
+//     alert("Daxil etdiyiniz bucaqlar yalnışdır")
+// }
+// TAPSIRIQ 1
+
+// var arr = [45,67,89,23,55,88,93]
+
+// for(i=0;i<arr.length;i++){
+//     console.log(arr[i]);
+// }
+// var xx = prompt('reqem daxil edin')
+// if(arr === xx){
+//     console.log("dogru");
+// }
+// else{
+//     console.log('sehv');
+// }
+
+// TAPSIRIQ 2 Massivdəki cüt ədədləri tapın. Massivdəki tək ədədləri tapın.
+
+// var arr = [678,45,67,89,23,545,188,293]
+
+// for(i=0; i<arr.length;i++){
+// if(i%2==0)
+//     console.log(arr[i]);
+// else if(i%2==1)
+//     console.log(arr[i]);
+// }
+
+// TAPSIRIQ 2 Bütün ədədlərin cəmini tapın.
+// var arr = [678,45,67,89,23,545,188,293]; 
+// var sum = 0; 
+// for (let i = 0; i < arr. length; i++) { 
+//     sum += arr[i];
+// } 
+//     console. log(sum);
+
+// TAPSIRIQ 2 Ədədlərin ədədi ortasını tapın.
+
+// var arr = [678,45,67,89,23,545,188,293]; 
+// var sum = 0; 
+// for (let i = 0; i < arr. length; i++) { 
+//     sum = (sum+arr[i])/arr.length
+// } 
+//     console. log(sum);
+
+//TAPSIRIQ 2  Massivdə 100dən boyük 200dən kiçik ədədlərin sayını tapın.
+// var arr = [678,45,67,89,23,545,188,293]
+
+// for(i=0; i<arr.length;i++){
+// if(i<200 & i>100)
+//     console.log(arr[i]);
+// }
+
+//TAPSIRIQ 2 Massivdəki ədədlərin sayını tapın.
+    // var arr = [678,45,67,89,23,545,188,293]
+    
+    // console.log(arr.length);
+
+
+
+
